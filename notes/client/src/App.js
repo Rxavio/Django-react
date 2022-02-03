f465add48130b7e976abded410fbe151bfcd78c7
xavioo <rucahoxavi@gmail.com>
2022-02-03 12:28:06 +0200

    start react frontend

---
+++ notes/client/src/App.js
@@ -0,0 +1,20 @@
+
+
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+   
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          My APP
+        </div>
+      </div>
+   
+  );
+}
+
+export default App;
+

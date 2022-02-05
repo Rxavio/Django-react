12009deca799f60dc966da3b1585a4e26854b2d3
xavioo <rucahoxavi@gmail.com>
2022-02-05 04:47:00 +0200

    fetch the notes from api and display on frontend react

--- notes/client/src/pages/NotesListPage.js
+++ notes/client/src/pages/NotesListPage.js
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 
 const NotesListPage = () => {
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+
+    let getNotes = async () => {
+
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let data = await response.json()
+        console.log(data)
+        setNotes(data)
+    }
 
     return (
         <div className="notes">
@@ -11,7 +24,9 @@ const NotesListPage = () => {
             </div>
 
             <div className="notes-list">
-               Notes
+                {notes.map((note, index) => (
+                    <h3 key={index}> {note.body} </h3>
+                ))}
             </div>
            
         </div>
